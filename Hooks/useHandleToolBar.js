1b2647807c89ff25aaf0788a0d82f9a1f63ff674
MukulKumar09 <mukulyashi@gmail.com>
2024-12-07 20:58:27 +0530

    delete confirm added

--- Hooks/useHandleToolBar.js
+++ Hooks/useHandleToolBar.js
@@ -5,6 +5,14 @@ import {useDispatch} from 'react-redux';
 import startPaste from '../Services/rnfs/startPaste';
 import askToRename from '../Services/askToRename';
 import moveItem from '../Services/rnfs/moveItem';
+import modalPromise from '../Actions/modalPromise';
+import Confirm from '../Layout/Modal/ModalBodies/Confirm';
+import collectHighilightedItems from '../Services/collectHighilightedItems';
+import MaterialIcon from '../Common/MaterialIcon/MaterialIcon';
+import {ScrollView, Text, View} from 'react-native';
+import styles from '../styles/styles';
+import useIcon from './useIcon';
+import SmallGrayText from '../Common/SmallGrayText/SmallGrayText';
 
 export default function useHandleToolBar(
   option,
@@ -29,8 +37,50 @@ export default function useHandleToolBar(
         startPaste(dispatch, state.clipboardItems, {...tab});
         break;
       }
+      case 'delete': {
+        async function asyncDelete() {
+          const highlightedItems = collectHighilightedItems(filesList);
+          const isConfirmDelete = await modalPromise(
+            dispatch,
+            Confirm,
+            {
+              description: (
+                <View style={[styles.bigGap]}>
+                  <Text style={[styles.text]}>
+                    Do you want to delete these {highlightedItems.length} items?
+                  </Text>
+                  {highlightedItems.map(item => (
+                    <View
+                      key={item.path}
+                      style={[styles.rowLayout, styles.mediumGap]}>
+                      {useIcon(item)}
+                      <View style={[styles.wide]}>
+                        <Text
+                          ellipsizeMode="tail"
+                          numberOfLines={1}
+                          style={[styles.text]}>
+                          {item.name}
+                        </Text>
+                        <SmallGrayText ellipsizeMode="tail" numberOfLines={2}>
+                          {item.path}
+                        </SmallGrayText>
+                      </View>
+                    </View>
+                  ))}
+                </View>
+              ),
+            },
+            {
+              icon: <MaterialIcon name="delete-outline" />,
+              heading: 'Confirm Delete?',
+            },
+          );
+        }
+        asyncDelete();
+        break;
+      }
       case 'rename': {
-        async function asyncFnc() {
+        async function asyncRename() {
           let lastHighlightedItem = {
             ...filesList.findLast(item => item.isHighlighted),
           };
@@ -44,7 +94,8 @@ export default function useHandleToolBar(
           dispatch({type: 'SETREFRESHPATH', payload: tab.path});
           dispatch({type: 'POPMODALSTACK'});
         }
-        asyncFnc();
+        asyncRename();
+        break;
       }
     }
     setOption('');
